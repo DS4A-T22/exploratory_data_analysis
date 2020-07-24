82f8c58bcf9aea95ed863b7180f34d1f5d1c7058
Leandro Ordonez Ante <leandro.ordonezante@ugent.be>
2020-07-25 00:45:24 +0200

    Add omnivida_util.py script: merge_on_closest_date()

---
+++ omnivida_util.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+def find_closest_date(df1_row, date_field_d1, df2, date_field_df2, filter_on='id_patient', add_time_delta_column=True):
+    deltas = np.abs(df2[df2[filter_on]==df1_row[filter_on]][date_field_df2] - df1_row[date_field_d1]).reset_index(drop=True)
+    #print(f"\n\nADHERENCE ROW: \n{df1_row}")
+    #print(f"\nDELTAS: \n{deltas}")
+    idx_closest_date = np.argmin(deltas)
+    #print(f"\nCLOSEST VALUE: \n{df2[df2[filter_on]==df1_row[filter_on]].iloc[idx_closest_date]}")
+    closest_date = df2[df2[filter_on]==df1_row[filter_on]].iloc[idx_closest_date][date_field_df2]
+    res = {"closest_date": closest_date}
+    idx = ['closest_date']
+    if add_time_delta_column:
+        res["closest_delta"] = (deltas[idx_closest_date] / np.timedelta64(1, 'D')) * (-1.0 if closest_date > df1_row[date_field_d1] else 1.0)
+        idx.append('closest_delta')
+    return pd.Series(res, index=idx)
+
+def merge_on_closest_date(df1, df2, date_field_d1, date_field_df2, merge_on='id_patient'):
+    temp_df1 = df1.copy()
+    temp_df1[[f'closest_{date_field_df2}', f'days_since_{date_field_df2}']] = temp_df1.apply(
+                                          find_closest_date, args=[date_field_d1, df2, date_field_df2, merge_on], axis=1)
+    
+    result_df = pd.merge(temp_df1, df2, left_on=[merge_on, f'closest_{date_field_df2}'], right_on=[merge_on, date_field_df2])
+    return result_df.drop(columns=f'closest_{date_field_df2}', axis=1)
